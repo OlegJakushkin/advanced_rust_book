14cd48530eb98f02b6e3145dd0ecafe087452f7b
Oleg Jakushkin <oleg.jakushkin@gmail.com>
2026-07-10 05:28:11 +0100

    Publish static site export for GitHub Pages.

---
+++ _next/static/D76k5h8aY0Bsw_E_OgzPp/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/advanced_rust_book//_next/:path+",
+        "destination": "/advanced_rust_book/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
